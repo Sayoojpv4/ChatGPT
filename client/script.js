540356cdca0e8612f06486bb13357adc709e6b6b
SAYOOJ P V <90135690+Sayoojpv4@users.noreply.github.com>
2023-07-02 18:30:21 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai-chatbot-eug0.onrender.com/', {
+    const response = await fetch('https://sayooj-x3yu.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
